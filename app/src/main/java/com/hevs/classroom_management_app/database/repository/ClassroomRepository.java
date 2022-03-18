9e8678c5344230c28e9fb64274a3cfc562189cbe
benjamin <benjamin.biollaz@students.hevs.ch>
2022-03-18 10:58:52 +0100

    application in repository

--- app/src/main/java/com/hevs/classroom_management_app/database/repository/ClassroomRepository.java
+++ app/src/main/java/com/hevs/classroom_management_app/database/repository/ClassroomRepository.java
@@ -5,12 +5,15 @@ import android.content.Context;
 
 import androidx.lifecycle.LiveData;
 
+import com.hevs.classroom_management_app.BaseApp;
 import com.hevs.classroom_management_app.database.AppDatabase;
 import com.hevs.classroom_management_app.database.async.classroom.CreateClassroom;
 import com.hevs.classroom_management_app.database.async.classroom.DeleteClassroom;
 import com.hevs.classroom_management_app.database.async.classroom.UpdateClassroom;
 import com.hevs.classroom_management_app.database.entity.Classroom;
 import com.hevs.classroom_management_app.util.OnAsyncEventListener;
+
+import java.util.List;
 
 public class ClassroomRepository {
     private static ClassroomRepository instance;
@@ -27,13 +30,18 @@ public class ClassroomRepository {
         return instance;
     }
 
-    public LiveData<Classroom> getById(final long id, Context context) {
-        return AppDatabase.getInstance(context).classroomDao().getById(id);
+    public LiveData<Classroom> getById(final long id, Application application) {
+        return  ((BaseApp)application).getDatabase().classroomDao().getById(id);
     }
 
-    public LiveData<Classroom> getByName(final String name, Context context) {
-        return AppDatabase.getInstance(context).classroomDao().getByName(name);
+    public LiveData<Classroom> getByName(final String name, Application application) {
+        return  ((BaseApp)application).getDatabase().classroomDao().getByName(name);
     }
+
+    public LiveData<List<Classroom>> getAll (Application application) {
+        return  ((BaseApp)application).getDatabase().classroomDao().getAll();
+    }
+
 
     public void insert(final Classroom classroom, OnAsyncEventListener callback, Application application) {
         new CreateClassroom(application, callback).execute(classroom);
