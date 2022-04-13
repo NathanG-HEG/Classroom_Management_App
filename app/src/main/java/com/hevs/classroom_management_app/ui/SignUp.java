d2fbb9ae4e87ea2cb3f6b02a688ad41deba2b381
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-04-13 17:07:24 +0200

    CHanged key of teacher by their emails

--- app/src/main/java/com/hevs/classroom_management_app/ui/SignUp.java
+++ app/src/main/java/com/hevs/classroom_management_app/ui/SignUp.java
@@ -88,8 +88,7 @@ public class SignUp extends AppCompatActivity {
     }
 
     private void createTeacher(Teacher teacher){
-        mAuth.signInWithEmailAndPassword(teacher.getEmail(), teacher.getPassword());
-        String key = mAuth.getCurrentUser().getUid();
+        String key = teacher.getEmail();
         teacherRepository.insert(teacher, key, new OnAsyncEventListener() {
             @Override
             public void onSuccess() {
